3c2e4455b6f5fc95613af337207f2010178b788c
Tarun Hasija <tarunhasija999@gmail.com>
2024-06-28 04:07:27 +0530

    new pattern done

---
+++ pattern/q9.java
@@ -0,0 +1,38 @@
+package pattern;
+
+import java.util.*;
+
+public class q9 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter value of n ");
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-1-i ; j++) {
+            System.out.print(" ");
+        }
+
+        for (int j = 0; j < 2*i+1; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 0; j <2*n - 1 - (2*i) ; j++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+
+    }
+    
+    
+    
+}
