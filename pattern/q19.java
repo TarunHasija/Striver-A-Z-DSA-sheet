b92d0497ad9287bf14ae9eb6b845aa8d4d30fb23
Tarun Hasija <tarunhasija999@gmail.com>
2024-06-28 20:40:06 +0530

    new pattern done

--- pattern/q19.java
+++ pattern/q19.java
@@ -8,25 +8,34 @@ public class q19 {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
-        for (int i = 0; i < 2*n; i++) {
-            for (int j = n; j >i ; j--) {
+        for (int i = 0; i <  n; i++) {
+            for (int j = n; j > i; j--) {
                 System.out.print("*");
             }
-            for (int j = 0; j < 2*(i); j++) {
+            for (int j = 0; j < 2 * (i); j++) {
                 System.out.print(" ");
             }
-            for (int j = n; j>i; j--) {
+            for (int j = n; j > i; j--) {
                 System.out.print("*");
-            }
-            if(i>n){
-                for (int j = 0; j <; j++) {
-                    
-                }
             }
 
             System.out.println();
 
-            
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print('*');
+            }
+
+            for (int j = 0; j < 2*(n-i-1); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
