b04efae2258b7be31ee10d6cf9eee5310e173ce1
Tarun Hasija <tarunhasija999@gmail.com>
2024-06-29 21:34:00 +0530

    new question added

--- mathsproblems/armstrong.java
+++ mathsproblems/armstrong.java
@@ -1,5 +1,27 @@
 package mathsproblems;
 
+import java.util.Scanner;
+
 public class armstrong {
-    
+    public static void main(String[] args) {
+        System.out.println("Enter value of n ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        n= Math.abs(n);
+        int digit;
+        int temp = n;
+        int sum = 0;
+        while (n>0) {
+            digit = n%10;
+            sum = sum + (digit*digit*digit);
+            n = n/10;
+        }
+        System.out.println(sum);
+        if (temp==sum) {
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
 }
---
+++ mathsproblems/print_all_divisor.java
@@ -0,0 +1,11 @@
+package mathsproblems;
+
+import java.util.Scanner;
+
+public class print_all_divisor {
+    public static void main(String[] args) {
+         System.out.println("Enter value of n ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+    }
+}
