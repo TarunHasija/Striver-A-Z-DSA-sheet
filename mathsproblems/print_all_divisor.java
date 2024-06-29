3d37dee6a1bdb56c1b39aea887055f1cd3ce9139
Tarun Hasija <tarunhasija999@gmail.com>
2024-06-29 21:42:38 +0530

    new question added

--- mathsproblems/print_all_divisor.java
+++ mathsproblems/print_all_divisor.java
@@ -1,5 +1,6 @@
 package mathsproblems;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class print_all_divisor {
@@ -7,5 +8,16 @@ public class print_all_divisor {
          System.out.println("Enter value of n ");
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
+
+
+        // * using ArrayList for adding value of divisor in the array
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        for (int i = 1; i <=n; i++) {
+            if (n%i==0) {
+                arrayList.add(i);
+            }
+        }
+        System.out.println(arrayList);
     }
 }
